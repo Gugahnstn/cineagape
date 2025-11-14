91084292873f4ec1aea2bba5c8b01d2de6ec7740
Gustavo Nogueira <gugahnstn>
2025-11-14 15:14:12 -0300

    commit finalmente

--- src/pages/Register/style.js
+++ src/pages/Register/style.js
@@ -108,7 +108,7 @@ export const Recontainer = styled.div`
     margin: 0 auto;
   }
 
-  @media (max-width: 400px) {
+  @media (max-width: 768px) {
     box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
     background-color: #FFFF;
     padding: 48px 0px;
